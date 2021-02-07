ac886c320aef0d42bdc920e640c1c4e2be72e34a
chenfinkel <finkelchen20@gmail.com>
2021-02-07 11:58:32 +0200

    poi page start

--- app/pages/points/pointsController.js
+++ app/pages/points/pointsController.js
@@ -1,9 +1,30 @@
 angular.module('pragueApp')
 .controller('pointsController', ['$location','$scope','$http','$window', '$rootScope', 'poiFactory', function ($location,$scope, $http, $window, $rootScope, poiFactory) {
-    $scope.point = {
-        imagePath: "https://www.prague-guide.co.uk/wp-content/uploads/2015/11/sasazu.jpg",
-        name: "SaSaZu"
-    } 
-    poiFactory("museums", $scope.point);
-    poiFactory("museums", $scope.point);
+
+    $scope.getAllPoints = function () {
+        var k = 0;
+        for (var i = 0; i < self.category.length; i++) {
+            $http({
+                url: self.serverUrl + "https://prague-advisor-server.herokuapp.com/poi/pointByCategory",
+                method: "GET",
+                params: { Category: self.category[i] }
+            })
+                .then(function (response) {
+                    if (response.data != "No points in this category") {
+                        result = response.data.result;
+                        for (i = 0; i < result.length; i++) {
+                            self.points[k] = {
+                                id: k,
+                                point: result[i],
+                                checked: false
+                            };
+                            k++;
+                        }
+                        self.pressCheck();
+                    }
+
+                }, function (response) {
+                });
+        }
+    }
 }]);
